499f14c079a49ff0b950331fde86e0f301c3f0dc
myuyen0304 <uyenuyen9780@gmail.com>
2024-08-14 15:15:48 +0700

    tui moi post file demo

---
+++ Quicksort.py
@@ -0,0 +1,32 @@
+#Doi gia tri cho 2 bien
+def doicho(m,n):
+    return n,m
+# Thuat toan quick sort
+def quicksort(L, R):
+    global a
+    if L >= R:
+        return
+    i = L
+    j = R
+    x = a[(L + R)//2]
+    while True:
+        while a[i]<x:
+            i = i+1
+        while a[j] > x:
+            j = j-1
+        if i <= j:
+            a[i], a[j] = doicho(a[i], a[j])
+            i = i+1
+            j = j-1
+        else:
+            break
+    quicksort(L, j)
+    quicksort(i, R)
+# Chuong trinh chinh 
+n = int(input("Nhap n ="))    
+a = []
+for i in range(0, n):
+    x = int(input("Nhap phan tu %d:" % (i+1)))
+    a.append(x)
+quicksort(0, n-1)
+print("Day so khi duoc sap xep lai: ",a)
